ce6d1a08bd12f569473ecf2db76537afb04b0872
root <nassimaskri@gmail.com>
2018-02-17 17:13:30 +0100

    yarn install

---
+++ config/database.js
@@ -0,0 +1,8 @@
+/**
+ * Created by nassim on 13/02/18.
+ */
+
+module.exports = {
+    'secret' : 'nassim',
+    'database': 'mongodb://localhost/myapp'
+}
---
+++ config/passport.js
@@ -0,0 +1,5 @@
+/**
+ * Created by nassim on 13/02/18.
+ */
+var jwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt')
